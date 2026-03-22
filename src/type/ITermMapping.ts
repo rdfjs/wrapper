245b1dcd3f2978a87370e13d131ed6687776478a
Samu Lang <langsamu@hotmail.com>
2026-03-22 16:20:39 +0000

    Simplify mapping from value to term
    1. It need not produce a wrapper but only a term, because it's used in a context that already has dataset & factory.
    2. It need not support undefined returns, because those are handled elsewhere in the transformation pipeline.

--- src/type/ITermMapping.ts
+++ src/type/ITermMapping.ts
@@ -1,6 +1,5 @@
-import type { DatasetCore, DataFactory } from "@rdfjs/types"
-import type { TermWrapper } from "../TermWrapper.js"
+import type { DataFactory, DatasetCore, Term } from "@rdfjs/types"
 
 export interface ITermMapping<T> {
-    (value: T, dataset: DatasetCore, factory: DataFactory): TermWrapper | undefined
+    (value: T, dataset: DatasetCore, factory: DataFactory): Term
 }
