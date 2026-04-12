782a1842ba3bc44e3b802588cc8764a83cb45fd7
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2026-04-12 22:35:01 +0100

    Refactor TermWrapper to use generics for improved type safety

--- src/TermWrapper.ts
+++ src/TermWrapper.ts
@@ -1,13 +1,13 @@
 import type { BaseQuad, DataFactory, DatasetCore, Literal, NamedNode, Term } from "@rdfjs/types"
 import type { IAnyTerm } from "./type/IAnyTerm.js"
 
-export class TermWrapper implements IAnyTerm {
-    private readonly original: Term
+type InferTerm<A> = A extends string ? NamedNode : A extends Term ? A : Term
 
-    public constructor(term: string, dataset: DatasetCore, factory: DataFactory)
-    public constructor(term: Term, dataset: DatasetCore, factory: DataFactory)
-    public constructor(term: string | Term, public readonly dataset: DatasetCore, public readonly factory: DataFactory) {
-        this.original = typeof term === "string" ? factory.namedNode(term) : term
+export class TermWrapper<T extends Term = Term> implements IAnyTerm {
+    private readonly original: T
+
+    public constructor(term: T extends NamedNode ? (string | T) : T, public readonly dataset: DatasetCore, public readonly factory: DataFactory) {
+        this.original = (typeof term === "string" ? factory.namedNode(term) : term) as T
     }
 
     get [Symbol.toStringTag]() {
@@ -52,5 +52,18 @@ export class TermWrapper implements IAnyTerm {
 
     equals(other: Term | null | undefined): boolean {
         return this.original.equals(other)
+    }
+
+    public static from<
+        This extends abstract new (...args: any[]) => TermWrapper,
+        A extends ConstructorParameters<This>[0]
+    >(this: This, term: A, dataset: DatasetCore, factory: DataFactory): InstanceType<This> & InferTerm<A> {
+        return new (this as any)(term, dataset, factory) as InstanceType<This> & InferTerm<A>
+    }
+
+    public static in<This extends abstract new (...args: any[]) => TermWrapper>(
+        this: This, dataset: DatasetCore, factory: DataFactory
+    ): <A extends ConstructorParameters<This>[0]>(term: A) => InstanceType<This> & InferTerm<A> {
+        return (term) => new (this as any)(term, dataset, factory) as any
     }
 }
